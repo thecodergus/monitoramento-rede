67d06045591049bebe708e69f91fa189a80be801
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-12-02 18:11:50 -0300

    att

--- codagem/target/debug/deps/codagem-1c1b9f83193c4746.d
+++
@@ -1,5 +0,0 @@
-C:\Users\gugap\monitoramento-rede\codagem\target\debug\deps\codagem-1c1b9f83193c4746.d: src\main.rs
-
-C:\Users\gugap\monitoramento-rede\codagem\target\debug\deps\libcodagem-1c1b9f83193c4746.rmeta: src\main.rs
-
-src\main.rs:
