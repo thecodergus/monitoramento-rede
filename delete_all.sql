bcf9ba8e110597862c4daffc34f5c6b5a131b1cf
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-12-03 01:16:40 -0300

    Ajustando SQL

---
+++ delete_all.sql
@@ -0,0 +1,5 @@
+DELETE FROM outage_events;
+DELETE FROM connectivity_metrics;
+DELETE FROM monitoring_cycles;
+DELETE FROM monitoring_probes;
+DELETE FROM monitoring_targets;
--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -77,46 +77,17 @@ CREATE INDEX idx_outage_duration ON outage_events (duration_seconds) WHERE durat
 
 -- Cloudflare DNS
 INSERT INTO monitoring_targets (name, address, asn, provider, type, region) VALUES
-('Cloudflare DNS', '1.1.1.1', 13335, 'Cloudflare', 'dns', 'global'),
-('Cloudflare DNS (Alt)', '1.0.0.1', 13335, 'Cloudflare', 'dns', 'global'),
-('Cloudflare DNS (IPv6)', '2606:4700:4700::1111', 13335, 'Cloudflare', 'dns', 'global'),
-('Cloudflare DNS (Alt IPv6)', '2606:4700:4700::1001', 13335, 'Cloudflare', 'dns', 'global'),
-
 -- Google Public DNS
 ('Google Public DNS', '8.8.8.8', 15169, 'Google', 'dns', 'global'),
 ('Google Public DNS (Alt)', '8.8.4.4', 15169, 'Google', 'dns', 'global'),
 ('Google Public DNS (IPv6)', '2001:4860:4860::8888', 15169, 'Google', 'dns', 'global'),
 ('Google Public DNS (Alt IPv6)', '2001:4860:4860::8844', 15169, 'Google', 'dns', 'global'),
 
--- Quad9 DNS
-('Quad9 DNS', '9.9.9.9', 19281, 'Quad9', 'dns', 'global'),
-('Quad9 DNS (Alt)', '149.112.112.112', 19281, 'Quad9', 'dns', 'global'),
-('Quad9 DNS (IPv6)', '2620:fe::fe', 19281, 'Quad9', 'dns', 'global'),
-('Quad9 DNS (Alt IPv6)', '2620:fe::9', 19281, 'Quad9', 'dns', 'global'),
-
 -- OpenDNS (Cisco Umbrella)
 ('OpenDNS', '208.67.222.222', 36692, 'Cisco', 'dns', 'global'),
 ('OpenDNS (Alt)', '208.67.220.220', 36692, 'Cisco', 'dns', 'global'),
 ('OpenDNS (IPv6)', '2620:119:35::35', 36692, 'Cisco', 'dns', 'global'),
 ('OpenDNS (Alt IPv6)', '2620:119:53::53', 36692, 'Cisco', 'dns', 'global'),
-
--- NextDNS
-('NextDNS', '45.90.28.0', 208552, 'NextDNS', 'dns', 'global'),
-('NextDNS (Alt)', '45.90.30.0', 208552, 'NextDNS', 'dns', 'global'),
-('NextDNS (IPv6)', '2a07:a8c0::', 208552, 'NextDNS', 'dns', 'global'),
-('NextDNS (Alt IPv6)', '2a07:a8c1::', 208552, 'NextDNS', 'dns', 'global'),
-
--- G-Core Labs DNS
-('G-Core Labs DNS', '95.85.95.85', 199524, 'G-Core', 'dns', 'global'),
-('G-Core Labs DNS (Alt)', '2.56.220.2', 199524, 'G-Core', 'dns', 'global'),
-('G-Core Labs DNS (IPv6)', '2a03:90c0:999d::1', 199524, 'G-Core', 'dns', 'global'),
-('G-Core Labs DNS (Alt IPv6)', '2a03:90c0:9992::1', 199524, 'G-Core', 'dns', 'global'),
-
--- Yandex.DNS
-('Yandex.DNS', '77.88.8.8', 13238, 'Yandex', 'dns', 'eurasia'),
-('Yandex.DNS (Alt)', '77.88.8.1', 13238, 'Yandex', 'dns', 'eurasia'),
-('Yandex.DNS (IPv6)', '2a02:6b8::feed:0ff', 13238, 'Yandex', 'dns', 'eurasia'),
-('Yandex.DNS (Alt IPv6)', '2a02:6b8:0:1::feed:0ff', 13238, 'Yandex', 'dns', 'eurasia'),
 
 -- Registro.br (NIC.br) - Todos servidores autoritativos IPv4 e IPv6
 ('Registro.br a.dns.br', '200.160.0.10', 22548, 'NIC.br', 'dns', 'br'),
@@ -130,8 +101,30 @@ INSERT INTO monitoring_targets (name, address, asn, provider, type, region) VALU
 ('Registro.br e.dns.br', '200.229.248.10', 22548, 'NIC.br', 'dns', 'br'),
 ('Registro.br e.dns.br (IPv6)', '2001:12f8:2::10', 22548, 'NIC.br', 'dns', 'br'),
 
--- Akamai Edge (CDN) - Use hostname, não IP fixo
-('Akamai Edge', 'a173-223-99-56.deploy.static.akamaitechnologies.com', NULL, 'Akamai', 'cdn', 'global'),
 
--- Amazon CloudFront (CDN) - Use hostname, não IP fixo
-('Amazon CloudFront', 'd111111abcdef8.cloudfront.net', NULL, 'AWS', 'cdn', 'global');
+-- SOUTH AMERICA (5 probes)
+INSERT INTO monitoring_probes (location, ip_address, provider) VALUES
+('São Paulo - AWS sa-east-1', '18.228.0.1', 'AWS'),
+('São Paulo - AWS sa-east-1 IPv6', '2600:1f18:6ff:f000::1', 'AWS'),
+('São Paulo - GCP southamerica-east1', '35.198.10.1', 'GCP'),
+('Rio de Janeiro - Datacenter Próprio', '200.160.10.1', 'Internal'),
+('Buenos Aires - DigitalOcean', '157.230.200.1', 'DigitalOcean');
+
+-- NORTH AMERICA (7 probes)
+INSERT INTO monitoring_probes (location, ip_address, provider) VALUES
+('Virginia - AWS us-east-1', '3.80.0.1', 'AWS'),
+('Virginia - AWS us-east-1 IPv6', '2600:1f18:400:8000::1', 'AWS'),
+('Oregon - GCP us-west1', '34.102.0.1', 'GCP'),
+('Texas - Azure South Central US', '20.185.0.1', 'Azure'),
+('New York - DigitalOcean', '157.230.123.1', 'DigitalOcean'),
+('Toronto - Linode', '172.105.25.1', 'Linode'),
+('Miami - CDN Edge', '23.236.0.1', 'CDN');
+
+-- WESTERN EUROPE (6 probes)
+INSERT INTO monitoring_probes (location, ip_address, provider) VALUES
+('Frankfurt - AWS eu-central-1', '18.184.0.1', 'AWS'),
+('Frankfurt - AWS eu-central-1 IPv6', '2a05:d018:400:8000::1', 'AWS'),
+('London - Azure UK South', '51.140.0.1', 'Azure'),
+('Amsterdam - GCP europe-west4', '35.204.0.1', 'GCP'),
+('Paris - Linode', '172.105.67.1', 'Linode'),
+('Dublin - CDN Edge', '54.246.0.1', 'CDN');
